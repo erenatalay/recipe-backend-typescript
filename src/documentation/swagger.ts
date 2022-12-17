65a8a419edb75fccda66ee47b450bab30e794df7
erenatalay <ahmetatalay95@gmail.com>
2022-12-17 17:46:16 +0300

    example swagger use

--- src/documentation/swagger.ts
+++ src/documentation/swagger.ts
@@ -6,9 +6,9 @@ export const options = {
       title: "Simple Server", // Swagger arayüzünün başlıkları
       version: "1.0.0", // Swagger'ın API'ın hangi sürümü için üretildiği
     },
-    host: "localhost", // API uygulamanızın host name'i yani endpointi
+    host: "localhost:3000", // API uygulamanızın host name'i yani endpointi
     swagger: "2.0", //  or openapi:'3.0.0' // Burada swagger veya openapi için tanımlamaları görüldüğü şekilde yapabilirsiniz
-    basePath: "/v1", // giriş path'i olarak eklenebilir
+    basePath: "/api", // giriş path'i olarak eklenebilir
     produces: [
       // API uygulamasının Üretebileceği veri tipleri
       "application/json",
@@ -26,5 +26,5 @@ export const options = {
     },
   },
   basedir: __dirname, //Swagger üretimi için temel alınacak dosyaların hepsi - Routelarınızın bulunduğu klasör olarak ayarlamalısınız
-  files: ["../routes/**/*.ts"], //Swagger üretimi
+  files: ["../controller/**/*.ts"], //Swagger üretimi
 };
