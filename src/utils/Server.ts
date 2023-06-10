a6d3d5d2f14f43dff364736230b660181f85e29d
erenatalay <ahmetatalay95@gmail.com>
2023-06-10 10:34:46 +0300

    docker prod and dev envoriment

--- src/utils/Server.ts
+++ src/utils/Server.ts
@@ -17,8 +17,5 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api", routers);
 app.use(errorHandler);
-app.listen(process.env.APP_PORT, () => {
-  console.log(`${process.env.APP_PORT} Port Server Start`);
-  app.use(errorHandler);
-});
+
 export default app;
